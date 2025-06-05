51f3673a4a10213eb3b1073278405b767563a6c9
Sowmiya Ravikumar <sowmiyathilakavathi@gmail.com>
2025-06-05 10:49:41 +0100

    more practice on easy array problems

---
+++ src/main/java/week1/arrays/MajorityElement.java
@@ -0,0 +1,52 @@
+package week1.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        int majorBF = majorityElementBruteForce(nums);
+        int majorOpt= majorityElementOptimized(nums);
+
+        System.out.printf("BF major %d and Optimized major %d", majorBF, majorOpt);
+    }
+
+    public static int majorityElementBruteForce(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i: nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int last_max_val = 0;
+        int last_max_key = 0;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            int val = entry.getValue();
+            if(val > last_max_val) {
+                last_max_val = val;
+                last_max_key = entry.getKey();
+            }
+        }
+        return last_max_key;
+    }
+
+    public static int majorityElementOptimized(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int major = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                major = num;
+            }
+            if (num == major) {
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+
+        return major;
+    }
+}
---
+++ src/main/java/week1/arrays/MoveZeros.java
@@ -0,0 +1,25 @@
+package week1.arrays;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+        int zero_ptr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[zero_ptr];
+                nums[zero_ptr] = tmp;
+                zero_ptr += 1;
+            }
+        }
+    }
+}
