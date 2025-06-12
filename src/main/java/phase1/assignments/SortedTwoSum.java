e60942becc394d47ef345f1676e2c044638d2adb
Sowmiya Ravikumar <sowmiyathilakavathi@gmail.com>
2025-06-12 22:21:37 +0100

    linkedlist practice 1 added

--- src/main/java/phase1/assignments/SortedTwoSum.java
+++ src/main/java/phase1/assignments/SortedTwoSum.java
@@ -7,18 +7,17 @@ public class SortedTwoSum {
 
         List< List<Integer>> result = new ArrayList<>();
         if(nums != null && nums.length > 1) {
-            Arrays.sort(nums);
 
             int left = 0, right = nums.length - 1;
             while(left < right) {
                 int sum = nums[left] + nums[right];
                 if(sum == target) {
                     List<Integer> interim_result = new ArrayList<>();
-                    interim_result.add(nums[left]);
-                    interim_result.add(nums[right]);
+                    interim_result.add(left);
+                    interim_result.add(right);
                     result.add(interim_result);
                     left += 1;
-                    right += 1;
+                    right -= 1;
                 } else {
                     if(sum > target) {
                         right -= 1;
@@ -34,7 +33,7 @@ public class SortedTwoSum {
 
     public static void main(String[] args) {
 
-        int[] nums = {2, 11, 7, 9, 3, 5, 4};
+        int[] nums = {2, 3, 4, 5, 7, 9, 11};
         int target = 9;
 
         List<List<Integer>> result = twoSum(nums, target);
---
+++ src/main/java/phase1/linkedlist/IntersectionPoint.java
@@ -0,0 +1,33 @@
+package phase1.linkedlist;
+
+import phase1.Node;
+
+public class IntersectionPoint {
+    public static void main(String[] args) {
+        Node l1 = Node.createLinkedList(new int[]{1, 9, 1}, -1);
+        Node l2 = Node.createLinkedList(new int[]{3, 2, 4}, -1);
+        Node tmp = l1;
+        while(tmp.next != null) {
+            tmp = tmp.next;
+        }
+        tmp.next = l2.next;
+
+        Node resBF = getIntersectionNodeBF(l1, l2);
+        System.out.println(resBF != null ? resBF.val : resBF);
+    }
+
+    static Node getIntersectionNodeBF(Node headA, Node headB) {
+        Node currentA = headA;
+        while(currentA != null) {
+            Node currentB = headB;
+            while(currentB != null) {
+                if(currentB == currentA) {
+                    return currentA;
+                }
+                currentB = currentB.next;
+            }
+            currentA = currentA.next;
+        }
+        return null;
+    }
+}
